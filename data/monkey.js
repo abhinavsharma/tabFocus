1394c547acaf8dba18bdc2225168e8baf598b10d
Abhinav Sharma <abhinav.sharma@yahoo.com>
2011-07-11 16:57:30 -0700

    creates a session tree

--- data/monkey.js
+++ data/monkey.js
@@ -1,19 +1,18 @@
-console.log("this is content script with no privileges");
 const reportError = console.log
 let $ = document.getElementById
 let C = document.createElement;
 let J = JSON.stringify;
 
 self.on("message", function(data) {
-  console.log(J(data.clusters));
+  //console.log(J(data.clusters));
   let allClusters = $('all-clusters');
   for (let idx in data.clusters) {
     let clusterElem = C('ul');
     clusterElem.setAttribute('id', 'cluster-' + idx);
     allClusters.appendChild(clusterElem);
-    data.clusters[idx].forEach(function ({id, title, url, rev_host}) {
+    data.clusters[idx].forEach(function ({id, title, url, rev_host, session}) {
       let el = C('li');
-      el.innerHTML = url;
+      el.innerHTML = title + " | " + session;
       clusterElem.appendChild(el);
     })
   }
